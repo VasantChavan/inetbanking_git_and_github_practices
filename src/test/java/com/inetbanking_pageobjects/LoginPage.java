5db07774b7ee9f0d56ed16d318f79f3dcc396f07
ASUS <ASUS@DESKTOP-OODQLTV>
2024-04-21 13:25:51 +0530

    updated loginpage,TestBase and LoginTc file

--- src/test/java/com/inetbanking_pageobjects/LoginPage.java
+++ src/test/java/com/inetbanking_pageobjects/LoginPage.java
@@ -1,12 +1,36 @@
 package com.inetbanking_pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
 	WebDriver driver;
 
+	@FindBy(name = "uid")
+	WebElement username_txt;
+
+	@FindBy(name = "password")
+	WebElement password_txt;
+
+	@FindBy(name = "btnLogin")
+	WebElement loginBtn;
+
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public void login(String username, String password) {
+		
+		username_txt.clear();
+		username_txt.sendKeys(username);
+
+		password_txt.clear();
+		password_txt.sendKeys(password);
+
+		loginBtn.click();
 	}
 }
--- src/test/java/com/inetbanking_testbase/TestBase.java
+++ src/test/java/com/inetbanking_testbase/TestBase.java
@@ -1,5 +1,40 @@
 package com.inetbanking_testbase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
 public class TestBase {
 
+	public static WebDriver driver;
+  
+	@BeforeSuite
+	public void init() {
+	
+	}
+	
+	@BeforeClass
+	public void initWebObject()  
+	{
+		
+    }
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/v4/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterTest
+	public void teardown() {
+	driver.quit();
+	}
 }
